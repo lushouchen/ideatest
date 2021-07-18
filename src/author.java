c1cd32bd94c3800aefeff5f7add56914347ab2ed
lushouchen <lushouchen@yuanfudao.com>
2021-07-18 12:06:59 +0800

    Initial commit

---
+++ src/author.java
@@ -0,0 +1,19 @@
+/**
+ * @(#)author.java, 7月 18, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+import com.sun.org.apache.xpath.internal.functions.FuncSubstring;
+
+/**
+ * @author lushouchen
+ */
+public class author {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/config.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)config.java, 7月 17, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author lushouchen
+ */
+public class config {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
